e1a26bd694bb08a6727f46e5831b2547838d8797
FranklinUmeObi <franklinumeobi@gmail.com>
2020-07-09 19:44:43 +0100

    Added AudioVisualisr Project

--- Projects/LearningFetch/script.js
+++ Projects/LearningFetch/script.js
@@ -1,50 +1,67 @@
-
+//------------------------------------------------------------------------------
 //My Variables
+//------------------------------------------------------------------------------
+
 let url = "https://jsonplaceholder.typicode.com/posts";
-//let url2 = "https://jsonplaceholder.typicode.com/posts?userId=1"
+let url2 = "https://jsonplaceholder.typicode.com/posts?userId=1";
+
+let myHeaders = {"Content-type": "application/json; charset=UTF-8"};
+
+let myObject = {title: "foo", body: "bar", userId: 1};
+let myObject2 = [
+  {title: "king", body: "first", userId: 1},
+  {title: "queen", body: "second", userId: 2},
+  {title: "jack", body: "third", userId: 3}
+];
 
 let optionsForPost = {
   method: "POST",
-  body: JSON.stringify({
-    title: "foo",
-    body: "bar",
-    userId: 1
-  }),
-  headers: {"Content-type": "application/json; charset=UTF-8"}
+  body: JSON.stringify(myObject2),
+  headers: myHeaders
 };
 
+//------------------------------------------------------------------------------
 //My Code
+//------------------------------------------------------------------------------
+
+/*
 GetRequest(url).catch(error => console.error(eror));
 PostRequest(url, optionsForPost).catch(error => console.error(eror));
 
+GetRequestManipulating(url).catch(error => console.error(eror));
+*/
 
+//------------------------------------------------------------------------------
+//Get Request
+//------------------------------------------------------------------------------
 
-
-
-
-//Get Request, data is in var json//orange words are just var names
-async function GetRequest(url){
-  const response =await fetch(url)
-  let data = await response.json()
-
-  console.log("This is the GET request response");
-  console.log(data);
-
-  console.log("This is a snippet of that data");
-  console.log(data[1]);
-
-  console.log("This is a snippet of that snippet");
-  console.log(data[1].id);
+async function GetRequest(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log("This is a snippet of the data : " + data[1].id);
   return data;
 }
 
 //------------------------------------------------------------------------------
 //Post Request
+//------------------------------------------------------------------------------
 
-async function PostRequest(url, options){
-  const response =await fetch(url, options)
-  let data = await response.json()
-  console.log("This is the POST request response");
+async function PostRequest(url, options) {
+  const response = await fetch(url, options);
+  let data = await response.json();
   console.log(data);
-  return data
+  return data;
+}
+
+//------------------------------------------------------------------------------
+//Get Request
+//------------------------------------------------------------------------------
+
+async function GetRequestManipulating(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+  let x = data;
+  console.log(x);
+  return data;
 }
